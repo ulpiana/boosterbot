7137b2c83af1a51561fbb5254f998b5a511cf2d7
ulpiana <ulpiana.b@hotmail.com>
2024-08-17 01:00:31 -0500

    who fucking knows

--- bot.js
+++ bot.js
@@ -1,43 +1,88 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const { Client, GatewayIntentBits, Events, PermissionsBitField, EmbedBuilder } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers] });
+const {
+    Client,
+    GatewayIntentBits,
+    EmbedBuilder,
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    Events
+} = require("discord.js");
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildPresences,
+  ],
+});
+const config = require("./config.json");
+const PREFIX = ".";
+const BOOSTER_ROLE_ID = "1174192479239688244"; // Replace with your server's booster role ID
 
-const PREFIX = '.';
-const BOOSTER_ROLE_ID = '1174192479239688244'; // Replace with your server's booster role ID
-
-client.on('ready', () => {
-    console.log(`Bot is online, ${client.user.tag}!`);
-  });
-
-
-client.on(Events.MessageCreate, async (oldMember, newMember, message, channel) => {
-    if (!message.guild || message.author.bot) return;
-
-    if (!oldMember.roles.cache.has(newMember.guild.roles.premiumSubscriberRole?.id)
-    && newMember.roles.cache.has(newMember.guild.roles.premiumSubscriberRole?.id)) {
-        const boosterAnnouncement = new EmbedBuilder()
-        .setAuthor({ name: 'Thanks for boosting the server!', iconURL: newMember.guild.iconURL({ size: 1024 })})
-        .setDescription(`${member.user.tag} just boosted ${message.guild.id}`)
-        .setColor(0x0099FF)
-        .setImage('https://64.media.tumblr.com/95f6e6a6a0cc5f8adb53c3f3c522bb9d/tumblr_mhqzex3FNw1rzn9loo1_500.gif')
-        
-}   const msg = await boostAnnounceChannel.send({
-    content: `${newMember} \`${newMember}\``,
-    embeds: [boosterAnnouncement],
-    
-  }); 
-
-    
+client.on("ready", () => {
+  console.log(`Bot is online, ${client.user.tag}!`);
+});
 
 
-    // if (message.content.startsWith(`${PREFIX}fall`)) {
-    //     const member = message.mentions.members.first();
-        
+
+
+client.on('guildMemberUpdate', (oldMember, newMember) => {
+    if (oldMember.premiumSince === null && newMember.premiumSince !== null) {
+        // User has boosted the server
+        const channel = client.channels.cache.get(CHANNEL_ID);
+        if (channel) {
+            const embed = new EmbedBuilder()
+                .setColor('#00FF00') // Green color for the embed
+                .setTitle('🚀 Server Boost!')
+                .setDescription(`${newMember.user.tag} has boosted the server! 🎉`)
+                .setTimestamp();
+
+            channel.send({ embeds: [embed] });
+        } else {
+            console.error('Channel not found');
+        }
+    }
+
+//   if (
+//     !oldMember.roles.cache.has(
+//       newMember.guild.roles.premiumSubscriberRole?.id
+//     ) &&
+//     newMember.roles.cache.has(newMember.guild.roles.premiumSubscriberRole?.id)
+//   ) {
+//     const boostAnnounceEmbed = new EmbedBuilder()
+//       .setAuthor({
+//         name: `Congratulations we have new booster!`,
+//         iconURL: newMember.guild.iconURL({ size: 1024 }),
+//       })
+//       .setDescription(
+//         `>>> Dear ${newMember}, You are really awesome and amazing.\nThanks for boosting our server, Enjoy your ${newMember.guild.roles.premiumSubscriberRole} role and other exclusive perks.`
+//       )
+
+//       .setImage(
+//         "https://cdn.discordapp.com/attachments/997473822787780669/1235649668978053181/p1_3187142_731753fb_LE_auto_x2_result.png?ex=663523ee&is=6633d26e&hm=7d7f3b33acb09f05a277802035cf84b79f8982eb1d01c73d0e8e60a85c23af78&"
+//       )
+//       .setColor("F47FFF")
+//       .setFooter({
+//         text: `${newMember.guild.name} Boost Detection System`,
+//         iconURL: newMember.user.displayAvatarURL({ size: 1024 }),
+//       })
+//       .setTimestamp();
+
+//     const msg = await boostAnnounceChannel.send({
+//       content: `${newMember} \`${newMember}\``,
+//       embeds: [boostAnnounceEmbed]
+
+//     });
+
+    // const member = message.mentions.members.first();
+
     //     if (!member) {
     //         return message.reply('Please mention a member to mute.');
     //     }
-        
+
     //     if (!message.member.roles.cache.has(BOOSTER_ROLE_ID)) {
     //         return message.reply('You do not have permission to use this command.');
     //     }
@@ -54,7 +99,7 @@ client.on(Events.MessageCreate, async (oldMember, newMember, message, channel) =
     //         .setTitle('Fall')
     //         .setDescription(`${member.user.tag} has been muted for 30 seconds`)
     //         .setImage('https://media1.tenor.com/m/bT-kVojT-X0AAAAC/adventure-time-fall.gif')
-            
+
     //         message.reply({ embeds: [newEmbed]});
     //     } catch (error) {
     //         console.error('Error muting member:', error);
@@ -86,12 +131,12 @@ client.on(Events.MessageCreate, async (oldMember, newMember, message, channel) =
     //     try {
     //         await member.setNickname(newNickname);
     //         // message.reply(`Changed ${member.user.tag}'s nickname to "${newNickname}".`);
-            // const newEmbed = new EmbedBuilder()
-            //     .setColor(0x0099FF)
-            //     .setTitle('Transfigured')
-            //     .setDescription(`${member.user.tag} has been transfigured`)
-            //     .setImage('https://64.media.tumblr.com/95f6e6a6a0cc5f8adb53c3f3c522bb9d/tumblr_mhqzex3FNw1rzn9loo1_500.gif')
-                
+    //         const newEmbed = new EmbedBuilder()
+    //             .setColor(0x0099FF)
+    //             .setTitle('Transfigured')
+    //             .setDescription(`${member.user.tag} has been transfigured`)
+    //             .setImage('https://64.media.tumblr.com/95f6e6a6a0cc5f8adb53c3f3c522bb9d/tumblr_mhqzex3FNw1rzn9loo1_500.gif')
+
     //             message.reply({ embeds: [newEmbed]});
     //     } catch (error) {
     //         console.error('Error changing nickname:', error);
@@ -102,10 +147,12 @@ client.on(Events.MessageCreate, async (oldMember, newMember, message, channel) =
     // const newEmbed = new EmbedBuilder()
     // .setColor(0x0099FF)
     // .setImage('https://media1.tenor.com/m/I1R3IIAKBRQAAAAC/gunther-gunther-adventure-time.gif')
-    
-    // message.send({ embeds: [newEmbed] });
-    // }
 
-    // https://media1.tenor.com/m/I1R3IIAKBRQAAAAC/gunther-gunther-adventure-time.gif
-});
+    // message.send({ embeds: [newEmbed] });
+  }
+
+  // https://media1.tenor.com/m/I1R3IIAKBRQAAAAC/gunther-gunther-adventure-time.gif
+);
+
+
 client.login(process.env.BOT_TOKEN); // Replace with your bot token
