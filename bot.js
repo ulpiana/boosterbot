5a29547e1b4a2b5b63cc94819d7009794eeb72c7
ulpiana <ulpiana.b@hotmail.com>
2025-03-15 20:06:58 -0500

    Disabling trivia for now

--- bot.js
+++ bot.js
@@ -66,124 +66,124 @@ function shuffleArray(array) {
   }
 }
 
-async function sendTriviaQuestion() {
-  const channel = client.channels.cache.get("945713353186234378"); // currently: NV general
+// async function sendTriviaQuestion() {
+//   const channel = client.channels.cache.get("1138596262673916008"); // currently: emotes general
 
-  if (triviaQuestions.length === 0) return;
-  const randomIndex = Math.floor(Math.random() * triviaQuestions.length);
-  const trivia = triviaQuestions[randomIndex];
+//   if (triviaQuestions.length === 0) return;
+//   const randomIndex = Math.floor(Math.random() * triviaQuestions.length);
+//   const trivia = triviaQuestions[randomIndex];
 
-  const questionEmbed = new EmbedBuilder()
-  .setAuthor({ name: "GOOD LUCK" })
-  .setColor("#ffba1e")
-  .setDescription(trivia.question)
-  .setImage(trivia.image)
-  .setTimestamp()
-  .setFooter({
-    text: "Boost the NIGHTVIBES server to gain access to exclusive commands and roles!",
-    iconURL: "https://cdn3.emoji.gg/emojis/2086-nitro-boost-spin.gif",
-  });
+//   const questionEmbed = new EmbedBuilder()
+//   .setAuthor({ name: "GOOD LUCK" })
+//   .setColor("#ffba1e")
+//   .setDescription(trivia.question)
+//   .setImage(trivia.image)
+//   .setTimestamp()
+//   .setFooter({
+//     text: "Boost the NIGHTVIBES server to gain access to exclusive commands and roles!",
+//     iconURL: "https://cdn3.emoji.gg/emojis/2086-nitro-boost-spin.gif",
+//   });
 
-  let answered = false; 
+//   let answered = false; 
 
-  const options = [
-    { label: trivia.option1, value: "option1" },
-    { label: trivia.option2, value: "option2" },
-    { label: trivia.option3, value: "option3" },
-  ];
+//   const options = [
+//     { label: trivia.option1, value: "option1" },
+//     { label: trivia.option2, value: "option2" },
+//     { label: trivia.option3, value: "option3" },
+//   ];
 
-  shuffleArray(options);
+//   shuffleArray(options);
 
-  const row = new ActionRowBuilder().addComponents(
-    options.map((option) =>
-      new ButtonBuilder()
-        .setCustomId(option.value)
-        .setLabel(option.label)
-        .setStyle(ButtonStyle.Primary)
-    )
-  );
+//   const row = new ActionRowBuilder().addComponents(
+//     options.map((option) =>
+//       new ButtonBuilder()
+//         .setCustomId(option.value)
+//         .setLabel(option.label)
+//         .setStyle(ButtonStyle.Primary)
+//     )
+//   );
 
-  const correctOption = trivia.correctAnswer;
-  const correctAnswer = options.find(
-    (option) => option.label === correctOption
-  ).value;
+//   const correctOption = trivia.correctAnswer;
+//   const correctAnswer = options.find(
+//     (option) => option.label === correctOption
+//   ).value;
 
-  const answerEmbed = new EmbedBuilder()
-  .setColor("#59a694")
-  .setTimestamp()
-  .setFooter({
-    text: "Thanks for answering!",
-  });
+//   const answerEmbed = new EmbedBuilder()
+//   .setColor("#59a694")
+//   .setTimestamp()
+//   .setFooter({
+//     text: "Thanks for answering!",
+//   });
 
-  const sentMessage = await channel.send({
-    embeds: [questionEmbed],
-    components: [row],
-    withResponse: true,
-  });
+//   const sentMessage = await channel.send({
+//     embeds: [questionEmbed],
+//     components: [row],
+//     withResponse: true,
+//   });
 
-  const collectorFilter = (i) => i.isButton();
+//   const collectorFilter = (i) => i.isButton();
 
-  const collector = channel.createMessageComponentCollector({
-    filter: collectorFilter,
-    time: 1_200_000, // 20 minutes
-  });
-  collector.on("collect", async (interaction) => {
-    const userAnswer = interaction.customId;
+//   const collector = channel.createMessageComponentCollector({
+//     filter: collectorFilter,
+//     time: 1_200_000, // 20 minutes
+//   });
+//   collector.on("collect", async (interaction) => {
+//     const userAnswer = interaction.customId;
 
-    if (userAnswer === correctAnswer) {
-      answered = true;
-      await interaction.update({
-        embeds: [
-          answerEmbed
-            .setAuthor({ name: "WOO-HOO!" })
-            .setDescription(`${interaction.user} got the correct answer! <:jake_exclamation:1290871949186039848>`),
-        ],
-        components: [],
-      });
-      collector.stop();
-    } else if (userAnswer !== correctAnswer) {
-      answered = true;
-      const member = await interaction.guild.members.fetch(interaction.user.id);
+//     if (userAnswer === correctAnswer) {
+//       answered = true;
+//       await interaction.update({
+//         embeds: [
+//           answerEmbed
+//             .setAuthor({ name: "WOO-HOO!" })
+//             .setDescription(`${interaction.user} got the correct answer! <:jake_exclamation:1290871949186039848>`),
+//         ],
+//         components: [],
+//       });
+//       collector.stop();
+//     } else if (userAnswer !== correctAnswer) {
+//       answered = true;
+//       const member = await interaction.guild.members.fetch(interaction.user.id);
   
-      // console.log(interaction.user);
-      await member.timeout(5_000);
-      await interaction.update({
-        embeds: [
-          answerEmbed
-            .setAuthor({ name: "WOMP WOMP" })
-            .setDescription(`${interaction.user} got the ANSWER WRONG! <:finn_uncomfy:959253867923664987>`)
-            .setColor("#ba1e36")
-            .setFooter({text: "They've been muted for 5 seconds :/"}),
-        ],
-        components: [],
-      });
-      collector.stop();
-    }
-  });
+//       // console.log(interaction.user);
+//       await member.timeout(5_000);
+//       await interaction.update({
+//         embeds: [
+//           answerEmbed
+//             .setAuthor({ name: "WOMP WOMP" })
+//             .setDescription(`${interaction.user} got the ANSWER WRONG! <:finn_uncomfy:959253867923664987>`)
+//             .setColor("#ba1e36")
+//             .setFooter({text: "They've been muted for 5 seconds :/"}),
+//         ],
+//         components: [],
+//       });
+//       collector.stop();
+//     }
+//   });
 
-  collector.on("end", async (reason) => {
-    if (!answered && reason !== 'time') {
-      await sentMessage.edit({
-        embeds: [
-          answerEmbed
-          .setAuthor({name: "TIME'S UP!"})
-          .setDescription("Looks like no one answered in time ")
-          .setFooter({text: "Try again next time :/"})
-          .setColor("#ffba1e"),
-        ],
-        components: [],
-      });
-    }
-  });
-};
+//   collector.on("end", async (reason) => {
+//     if (!answered && reason !== 'time') {
+//       await sentMessage.update({
+//         embeds: [
+//           answerEmbed
+//           .setAuthor({name: "TIME'S UP!"})
+//           .setDescription("Looks like no one answered in time ")
+//           .setFooter({text: "Try again next time :/"})
+//           .setColor("#ffba1e"),
+//         ],
+//         components: [],
+//       });
+//     }
+//   });
+// };
 
 // Bot's online
 client.once(Events.ClientReady, (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
   client.user.setActivity('It Came From The Nightosphere', { type: ActivityType.Watching });
-  setInterval(async () => {
-    await sendTriviaQuestion();
-}, 25_200_000);    //  7 hrs = 25_200_000
+//   setInterval(async () => {
+//     await sendTriviaQuestion();
+// }, 30_000);    //  7 hrs = 25_200_000
 
 });
 
